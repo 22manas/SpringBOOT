d0b76042eec5277905ab3b86cb8d647e767c7cc0
22manas <er.manaspatel@gmail.com>
2017-10-02 23:48:57 +0530

    initial commit

---
+++ my_workspace/myfirstspringbootproject/src/main/java/myfirstspringbootproject/CourseApiApp.java
@@ -0,0 +1,14 @@
+package myfirstspringbootproject;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
